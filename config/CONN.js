6c10c15bbaee12ced8a13de669951d2e2a3f5a68
Isiarf <imrfauntleroy@yahoo.com>
2024-02-12 21:23:10 -0800

    Server wont connect

--- config/CONN.js
+++ config/CONN.js
@@ -1,8 +1,20 @@
 const { connect, connection } = require('mongoose');
 require('dotenv').config();
 
-connect(process.env.MONGOSRV);
+(async () => {
+    if (connection.readyState !== 1) {
+        try {
+            await connect(process.env.MONGOSRV);
+            console.log("Database connected!");
+        } catch (error) {
+            console.error("Error connecting to database:", error.message);
+        }
+    } else {
+        console.log("Database already connected.");
+    }
+})();
 
 module.exports = connection;
+
 
 // config/CONN.js
